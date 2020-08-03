4054ad945cb2c70469cbd02f2f5ef5517fe47928
github_Pinchukctac <pinchukctac11@gmail.com>
2020-08-03 19:36:37 -0400

    added index page, css styling, and JS file for the index page

---
+++ Develop/public/js/script.js
@@ -0,0 +1,33 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    
+    burger.addEventListener('click', ()=>{
+        // toggle NaV
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index)=>{
+            if(link.style.animation) {
+                link.syle.animation = ''
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+            }
+        });
+    });
+
+}
+
+navSlide();
+
+// JavaScript Document
+// $(document).ready(function() {
+//     $('#autoWidth').lightSlider({
+//         autoWidth:true,
+//         loop:true,
+//         onSliderLoad: function() {
+//             $('#autoWidth').removeClass('cS-hidden');
+//         } 
+//     });  
+//   });
